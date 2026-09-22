874a1d78b927545a718782a23ebac20eaa575e80
Dhairya1981 <Dhairya.33495@stu.upes.ac.in>
2026-09-22 12:48:24 +0530

    Day20-30

---
+++ Day26q2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int lines[] = {1, 3, 5, 3, 1};
+    
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < lines[i]; j++) {
+            printf("*\n");
+        }
+        printf("\n");
+    }
+    
+return 0;
+}
